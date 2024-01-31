ca74a3f4e4f2d9cd7efded8436ec5d615babb108
Arpit Saxena <arpit.saxena2000@yahoo.in>
2024-01-31 23:31:54 +0530

    Add heartbeat interval
    Currently doing nothing on the hearbeat interval, ideally we will send
    heartbeat to the candidates. Should also use a precondition on the
    branch on the node being a leader or not

--- src/consensus/node.rs
+++ src/consensus/node.rs
@@ -7,7 +7,7 @@ use rand::distributions::{Distribution, Uniform};
 use rand::rngs::ThreadRng;
 use rand::thread_rng;
 use serde::{Deserialize, Serialize};
-use tokio::time::{Duration, Instant, Sleep};
+use tokio::time::{Duration, Instant, Interval, Sleep};
 use tracing::trace;
 
 type Message = Vec<u8>;
@@ -47,6 +47,7 @@ mod state {
 pub struct Node {
     persistent_state: state::Persistent,
     election_timer: Pin<Box<Sleep>>,
+    heartbeat_interval: Pin<Box<Interval>>,
 
     timer_distribution: Uniform<f32>,
     rng: ThreadRng,
@@ -54,11 +55,14 @@ pub struct Node {
 }
 
 /// Configuration for Raft Consensus, can be read from any file, currently only RON is supported
+#[serde_with::serde_as]
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     persistent_state_file: String,
     cluster_members: Vec<SocketAddr>,
     election_timeout_interval: RangeInclusive<f32>,
+    #[serde_as(as = "serde_with::DurationMilliSeconds")]
+    heartbeat_interval: Duration,
 }
 
 impl Config {
@@ -73,12 +77,15 @@ impl Config {
 impl Node {
     pub fn new(config_path: &str) -> Result<Node, Box<dyn std::error::Error>> {
         let config = Config::from_file(config_path)?;
+        let mut heartbeat_interval = Box::pin(tokio::time::interval(config.heartbeat_interval));
+        heartbeat_interval.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Delay);
 
         let distribution = Uniform::from(config.election_timeout_interval.clone());
         let rng = thread_rng();
         let mut node = Node {
             persistent_state: state::Persistent::new(),
             election_timer: Box::pin(tokio::time::sleep(Duration::from_secs(0))),
+            heartbeat_interval,
             timer_distribution: distribution,
             rng,
             config,
@@ -99,11 +106,15 @@ impl Node {
     // TODO: Should this be async?
     pub async fn tick(self: &mut Self) {
         // Tick the election timer
-        let timer = self.election_timer.as_mut();
+        let election_timer = self.election_timer.as_mut();
         tokio::select! {
-            () = timer => {
+            () = election_timer => {
                 trace!("Hey the election timer just expired do something about it");
                 self.reset_election_timer();
+            }
+            _ = self.heartbeat_interval.tick() => {
+                trace!("Heartbeat interval expired send heartbeat");
+                // TODO: Send heartbeat
             }
         };
     }
